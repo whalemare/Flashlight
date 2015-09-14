c62d1225536ad48a4d7294708801e0aee4b26db4
whalemare <nanostet@gmail.com>
2015-09-14 14:22:52 +0600

    Немного рефракторинга + так и остался баг, когда вспышка отрабатывает всего 2 нажатия. Ищу ошибку

--- app/src/main/java/ru/whalemare/flashlight/MainActivity.java
+++ app/src/main/java/ru/whalemare/flashlight/MainActivity.java
@@ -3,6 +3,7 @@ package ru.whalemare.flashlight;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,7 @@ public class MainActivity extends Activity {
 
     ImageView imageView;
     boolean hasFlash; // флаг поддержки устройством камеры
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,62 +27,57 @@ public class MainActivity extends Activity {
         // Проверяем поддержку вспышки в камере
         hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
-        if (!hasFlash)
+        if (!hasFlash) // Если камеры нет TODO открывать другое активити со светлым экраном.
+                        // TODO + запомнить, что если нет фонарика, то всегда будет открывтаься оно
             Toast.makeText(getApplicationContext(), "Камеры нет", Toast.LENGTH_LONG).show();
-
-        getCamera();
+        else
+            Toast.makeText(getApplicationContext(), "Камера есть", Toast.LENGTH_SHORT).show();
     }
 
-    private Camera camera;
     private Camera.Parameters params;
     private boolean isFlashOn;
-
-    private void getCamera()
-    {
+    public void getCamera() {
         if (camera == null)
-        {
             try {
                 camera = Camera.open();
                 params = camera.getParameters();
             } catch (RuntimeException e) {
-                Log.e("CAMERA NE RABOTAIT!11", e.getMessage());
+                e.printStackTrace();
             }
-        }
     }
 
-    private void turnOnFlash()
-    {
-        if (!isFlashOn){ // если вспышка выключена
-            if (camera == null || params == null){
+    private void turnOnFlash() {
+        if (!isFlashOn) { // если вспышка выключена
+            if (camera == null || params == null)
                 return;
-            }
-            params = camera.getParameters();
-            params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            camera.setParameters(params);
-            camera.startPreview();
-            imageView.setImageResource(R.drawable.flashon); // меняем картинку
-            isFlashOn = true;
+            else {
+                params = camera.getParameters();
+                params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+                camera.setParameters(params);
+                camera.startPreview(); // ?
 
-            Toast.makeText(getApplicationContext(), "Камера включена", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.flashon); // меняем картинку
+                isFlashOn = true;
+
+                Log.d("WHALETAG", "turnOnFlash() - камера включена" + params.getFlashMode().toString());
+            }
         }
-        else
-            Log.d("WHALETAG", "turnOnFlash() - камера включена");
     }
 
     private void turnOffFlash()
     {
         if (isFlashOn){
-            if (camera == null || params == null)
-                return;
-            params = camera.getParameters();
-            params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-            camera.setParameters(params);
-            camera.stopPreview();
-            camera.release();
-            imageView.setImageResource(R.drawable.flashoff); // меняем рисунок
-            isFlashOn = false;
+            if (camera != null) {
+                params = camera.getParameters();
+                params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                camera.setParameters(params);
+                camera.stopPreview(); // ?
 
-            Toast.makeText(getApplicationContext(), "Камера выключена", Toast.LENGTH_SHORT).show();
+                imageView.setImageResource(R.drawable.flashoff); // меняем рисунок
+                isFlashOn = false;
+
+                Log.d("WHALETAG", "turnOffFlash() - камера выключена -> " + params.getFlashMode().toString());
+            }
         }
     }
 
@@ -92,19 +89,9 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        turnOffFlash();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
+    protected void onStart() {
+        super.onStart();
+        getCamera(); // получаем параметры камеры
     }
 
     @Override
@@ -115,9 +102,9 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        getCamera(); // получаем параметры камеры
+    protected void onPause() {
+        super.onPause();
+    //    turnOffFlash();
     }
 
     @Override
@@ -128,5 +115,10 @@ public class MainActivity extends Activity {
             camera.release();
             camera = null;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
